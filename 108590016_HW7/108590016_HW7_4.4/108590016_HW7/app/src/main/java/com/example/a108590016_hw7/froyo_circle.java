0b922622c62158c7aa3bbe4a14fdb9e17ef4f977
t108590016 <77453397+t108590016@users.noreply.github.com>
2021-04-18 14:01:43 +0800

    HW7

---
+++ 108590016_HW7/108590016_HW7_4.4/108590016_HW7/app/src/main/java/com/example/a108590016_hw7/froyo_circle.java
@@ -0,0 +1,14 @@
+package com.example.a108590016_hw7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class froyo_circle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_froyo_circle);
+    }
+}
